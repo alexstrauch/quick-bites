895cedea2a26608f50ca59185b637bd18633defe
Alexander Strauch <strauch.alexander84@gmail.com>
2024-10-09 11:16:43 +0000

    Add RecipeGenerator with recipe file loading

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 import json
 import random
 
+class RecipeGenerator:
+    def __init__(self, recipe_file):
+        """
+        Initialize RecipeGenerator with a recipe file.
+        """
+        self.recipe_file = recipe_file
+        self.recipes = self.load_recipes()
+
+    
